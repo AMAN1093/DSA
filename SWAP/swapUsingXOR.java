7de01ee4a8c8cc6c2936cd104fe98c3c61144d99
AMAN1093 <amanberry2188@gmail.com>
2025-06-26 23:43:02 +0530

    swap using XOR operation between the two variable

---
+++ SWAP/swapUsingXOR.java
@@ -0,0 +1,17 @@
+package SWAP;
+
+public class swapUsingXOR {
+    public static void main(String[] args) {
+
+        int a = 10, b = 20;
+        System.out.println("initial a: "+ a+" b: "+b);
+        
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("final a: "+ a+" b: "+b);
+        
+
+    }
+
+}
