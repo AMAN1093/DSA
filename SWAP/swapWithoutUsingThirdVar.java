7bbf22888b0cad9cd949b7b57ce9bced74682759
AMAN1093 <amanberry2188@gmail.com>
2025-06-26 23:37:25 +0530

    swapping without and with using third variable

---
+++ SWAP/swapWithoutUsingThirdVar.java
@@ -0,0 +1,14 @@
+package SWAP;
+
+public class swapWithoutUsingThirdVar {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("initial a: "+ a+" b: "+b);
+        a = a + b; 
+        b = a - b; 
+        a = a - b; 
+        System.out.println("final a: "+ a+" b: "+b);
+
+    }
+
+}
